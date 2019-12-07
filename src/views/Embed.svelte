f5a01b7774c63b5afd7cd97ea27a884b7d16b4ac
spiffytech <spiffy@spiffy.tech>
2019-12-07 10:19:29 -0500

    Start showing HTML version of self text. Still need to style it.

--- src/views/Embed.svelte
+++ src/views/Embed.svelte
@@ -1,4 +1,6 @@
 <script>
+  import unescape from 'lodash/unescape';
+
   import * as libreddit from "../lib/reddit";
 
   export let article;
@@ -9,7 +11,7 @@
 {#if embedType === 'image'}
   <img src={article.url} alt={article.title} />
 {:else if embedType === 'self'}
-  <p class="bg-white rounded-lg p-3">{article.selftext}</p>
+  <p class="bg-white rounded-lg p-3">{@html unescape(article.selftext_html)}</p>
 {:else if embedType === 'link'}
   <a href={article.url} target="blank">🌎{article.title}</a>
 {:else if embedType === 'video'}
